d4d78ffeb235e7fee40339dbb3da43919e84134b
aryalaashu <aryalaashutosh@gmail.com>
2023-03-21 17:32:59 +0545

    brmetal

---
+++ script.js
@@ -0,0 +1,56 @@
+// menu bar
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+
+
+// contact us
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+// scroll to top
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+// popup
+// window.addEventListener("scroll", function() {
+//   var popup = document.getElementById("review");
+//   var position = window.scrollY + window.innerHeight;
+//   var target = document.documentElement.scrollHeight;
+//   if (position > target * 0.8) {
+//     popup.style.display = "block";
+//   }
+// });
